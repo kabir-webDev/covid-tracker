0670aba23a766b7e828d7308d9079cf12e53c988
kabir-webDev <kabir.webdev@gmail.com>
2020-10-18 00:03:17 +0600

    Updates

---
+++ precache-manifest.5416e6e4a6f7be6a89fbb0d292c97b5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33dd24e828a5b8905dde32092e41705d",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "992b49572a9a588b71b4",
+    "url": "/covid-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "be0606e1056c587cc5ce",
+    "url": "/covid-tracker/static/css/main.7e9abd69.chunk.css"
+  },
+  {
+    "revision": "992b49572a9a588b71b4",
+    "url": "/covid-tracker/static/js/2.f7e31907.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid-tracker/static/js/2.f7e31907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be0606e1056c587cc5ce",
+    "url": "/covid-tracker/static/js/main.3c75528d.chunk.js"
+  },
+  {
+    "revision": "1af475054be274cc1e5c",
+    "url": "/covid-tracker/static/js/runtime-main.9cfae146.js"
+  }
+]);
